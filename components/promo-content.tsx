23c29ffabe4db2c8fb16a5d261efba4311a18595
Morteza Aghaei <mi.piboy.org@gmail.com>
2026-01-02 03:32:05 +0330

    fix error

--- components/promo-content.tsx
+++ components/promo-content.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
-import { cn } from "@/lib/utils";
+// import React from "react";
+// import { cn } from "@/lib/utils";
 
 interface PromoContentProps {
   variant?: "desktop" | "mobile";
